ec8b657c8efe853c09de62d9b52e92b40b46ddce
Rahulio <rahul.ramesh888@gmail.com>
2019-01-31 20:46:58 -0500

    Add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
+class App extends Component<{}, {}> {
   render() {
     return (
-      <div className="App">
+      <div className="App bg-blue">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
